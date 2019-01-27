bab18526a6a9bea80bef54aee798c9c83f5f1adc
cyberpwn <danielmillst@gmail.com>
2019-01-27 14:26:45 -0500

    Sampler interface

---
+++ src/main/java/com/volmit/react/api/RSampler.java
@@ -0,0 +1,55 @@
+package com.volmit.react.api;
+
+public class RSampler implements ISampler
+{
+
+	@Override
+	public String getId()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getShortcode()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDisplayName()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sample()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double getValue()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String get()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

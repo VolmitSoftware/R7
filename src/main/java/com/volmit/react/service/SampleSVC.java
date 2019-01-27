e384c99e3e87199c184d52d288d861360ed3cb2d
cyberpwn <danielmillst@gmail.com>
2019-01-27 14:26:18 -0500

    Sampler service

---
+++ src/main/java/com/volmit/react/service/SampleSVC.java
@@ -0,0 +1,29 @@
+package com.volmit.react.service;
+
+import com.volmit.react.api.RSVC;
+
+public class SampleSVC extends RSVC
+{
+	public SampleSVC()
+	{
+		super(true, 0, 0);
+	}
+
+	@Override
+	public void onStart()
+	{
+
+	}
+
+	@Override
+	public void onStop()
+	{
+
+	}
+
+	@Override
+	public void onTick()
+	{
+
+	}
+}

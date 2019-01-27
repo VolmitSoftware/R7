15fa726c572803108391a826e65ab102dffd203f
cyberpwn <danielmillst@gmail.com>
2019-01-27 13:01:22 -0500

    Main

---
+++ src/main/java/com/volmit/react/React.java
@@ -0,0 +1,29 @@
+package com.volmit.react;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class React extends JavaPlugin
+{
+	public React()
+	{
+		super();
+	}
+
+	@Override
+	public void onLoad()
+	{
+
+	}
+
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
+}

b92ca2e942f1b23278ce7d3bea82d1365644c962
cyberpwn <danielmillst@gmail.com>
2019-01-27 14:25:51 -0500

    React ticking

--- src/main/java/com/volmit/react/React.java
+++ src/main/java/com/volmit/react/React.java
@@ -1,5 +1,6 @@
 package com.volmit.react;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class React extends JavaPlugin
@@ -18,12 +19,25 @@ public class React extends JavaPlugin
 	@Override
 	public void onEnable()
 	{
-
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> tick(), 0, 0);
 	}
 
 	@Override
 	public void onDisable()
 	{
 
+	}
+
+	private void tick()
+	{
+		try
+		{
+
+		}
+
+		catch(Throwable e)
+		{
+			e.printStackTrace();
+		}
 	}
 }

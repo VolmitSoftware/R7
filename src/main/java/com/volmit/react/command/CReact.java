aecc50062a9fe8f5fc8b2a2ba9e7a051d18a1cb3
cyberpwn <danielmillst@gmail.com>
2019-01-28 13:27:42 -0500

    Monitor command

--- src/main/java/com/volmit/react/command/CReact.java
+++ src/main/java/com/volmit/react/command/CReact.java
@@ -1,10 +1,14 @@
 package com.volmit.react.command;
 
 import com.volmit.react.Gate;
+import com.volmit.volume.bukkit.command.Command;
 import com.volmit.volume.bukkit.command.VolumeSender;
 
 public class CReact extends ReactCommand
 {
+	@Command
+	public CMonitor cMonitor;
+
 	public CReact()
 	{
 		super("react", "re");

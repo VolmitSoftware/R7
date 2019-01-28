82803236093ab9de99e351b83ff4bfcb7bc5d684
cyberpwn <danielmillst@gmail.com>
2019-01-28 12:51:04 -0500

    Monitor svc

--- src/main/java/com/volmit/react/service/MonitorSVC.java
+++ src/main/java/com/volmit/react/service/MonitorSVC.java
@@ -1,6 +1,9 @@
 package com.volmit.react.service;
 
+import com.volmit.react.React;
 import com.volmit.react.api.RSVC;
+import com.volmit.react.api.ReactPlayer;
+import com.volmit.react.util.nms.NMP;
 
 public class MonitorSVC extends RSVC
 {
@@ -12,7 +15,23 @@ public class MonitorSVC extends RSVC
 	@Override
 	public void onStart()
 	{
+		for(ReactPlayer i : React.playerSVC.getPlayers())
+		{
+			if(i.isMonitoring())
+			{
+				tickMonitor(i);
+			}
+		}
+	}
 
+	private void tickMonitor(ReactPlayer i)
+	{
+		NMP.MESSAGE.action(i.player(), getMonitor(i));
+	}
+
+	private String getMonitor(ReactPlayer i)
+	{
+		return React.sampleSVC.getSampler("tps").get();
 	}
 
 	@Override

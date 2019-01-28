6b46c08a508a98b172239747b4ef718ba6e58d14
cyberpwn <danielmillst@gmail.com>
2019-01-28 11:52:09 -0500

    Monitor Service

---
+++ src/main/java/com/volmit/react/service/MonitorSVC.java
@@ -0,0 +1,29 @@
+package com.volmit.react.service;
+
+import com.volmit.react.api.RSVC;
+
+public class MonitorSVC extends RSVC
+{
+	public MonitorSVC()
+	{
+		super(true, 0, 15);
+	}
+
+	@Override
+	public void onStart()
+	{
+
+	}
+
+	@Override
+	public void onStop()
+	{
+
+	}
+
+	@Override
+	public void onTick()
+	{
+
+	}
+}

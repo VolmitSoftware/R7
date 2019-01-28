54d94eced8bfad8767a072dbb3b3a66e6bce1ef3
cyberpwn <danielmillst@gmail.com>
2019-01-28 06:26:07 -0500

    Impl rsamp

--- src/main/java/com/volmit/react/api/RSampler.java
+++ src/main/java/com/volmit/react/api/RSampler.java
@@ -1,55 +1,115 @@
 package com.volmit.react.api;
 
-public class RSampler implements ISampler
+public abstract class RSampler implements ISampler
 {
+	private volatile double value;
+	private final String id;
+	private String shortcode;
+	private String displayName;
+	private String description;
+	private int accuracy;
+
+	public RSampler(String id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * Set the shortcode
+	 *
+	 * @param shortcode
+	 *            the shortcode
+	 */
+	protected void setShortcode(String shortcode)
+	{
+		this.shortcode = shortcode;
+	}
+
+	/**
+	 * Set the display name
+	 *
+	 * @param displayName
+	 *            the display name
+	 */
+	protected void setDisplayName(String displayName)
+	{
+		this.displayName = displayName;
+	}
+
+	/**
+	 * Set the description
+	 *
+	 * @param description
+	 *            the description
+	 */
+	protected void setDescription(String description)
+	{
+		this.description = description;
+	}
 
 	@Override
 	public String getId()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
 
 	@Override
 	public String getShortcode()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return shortcode;
 	}
 
 	@Override
 	public String getDisplayName()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return displayName;
 	}
 
 	@Override
 	public String getDescription()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return description;
 	}
 
 	@Override
-	public void sample()
-	{
-		// TODO Auto-generated method stub
+	public abstract void sample();
 
+	/**
+	 * Format a value. Do not apply the short code, or apply any coloring. This is
+	 * computed later.
+	 *
+	 * @param v
+	 *            the value
+	 * @return the formatted value.
+	 */
+	public abstract String format(double v);
+
+	public void setValue(double v)
+	{
+		this.value = v;
 	}
 
 	@Override
 	public double getValue()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return value;
 	}
 
 	@Override
 	public String get()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return format(getValue());
 	}
 
+	@Override
+	public void setAccuracy(int accuracy)
+	{
+		this.accuracy = accuracy;
+	}
+
+	@Override
+	public int getAccuracy()
+	{
+		return accuracy;
+	}
 }

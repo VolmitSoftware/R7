cdc5dd572ca37044294989c6068f5d273b5675a3
cyberpwn <danielmillst@gmail.com>
2019-01-28 09:40:10 -0500

    Gate messages

---
+++ src/main/java/com/volmit/react/Gate.java
@@ -0,0 +1,50 @@
+package com.volmit.react;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.volmit.react.nms.FrameType;
+import com.volmit.react.nms.NMP;
+import com.volmit.react.nms.NMSVersion;
+
+public class Gate
+{
+	public static final String TAG = ChatColor.BLUE + "[" + ChatColor.DARK_GRAY + ChatColor.BOLD + "React" + ChatColor.RESET + ChatColor.BLUE + "]" + ChatColor.GRAY + ": ";
+	public static final String TAG_NP = ChatColor.BLUE + "[" + ChatColor.DARK_GRAY + ChatColor.BOLD + "React" + ChatColor.RESET + ChatColor.BLUE + "]" + ChatColor.GRAY;
+
+	public static void msg(CommandSender s, String message)
+	{
+		s.sendMessage(TAG + message);
+	}
+
+	public static void notifSuccess(Player p, String message)
+	{
+		notif(p, message, new ItemStack(Material.SLIME_BALL));
+	}
+
+	public static void notifWarn(Player p, String message)
+	{
+		notif(p, message, new ItemStack(Material.MAGMA_CREAM));
+	}
+
+	public static void notifFail(Player p, String message)
+	{
+		notif(p, message, new ItemStack(Material.BARRIER));
+	}
+
+	public static void notif(Player p, String message, ItemStack is)
+	{
+		if(NMSVersion.R1_9_2.betweenInclusive(NMSVersion.R1_13).contains(NMSVersion.current()))
+		{
+			NMP.MESSAGE.advance(p, is, TAG + "\n" + ChatColor.GRAY + message, FrameType.GOAL);
+		}
+
+		else
+		{
+			msg(p, message);
+		}
+	}
+}

8ce8f023e20aa638af81710c53a8ffab46f08573
cyberpwn <danielmillst@gmail.com>
2019-01-28 11:53:23 -0500

    Player svc

---
+++ src/main/java/com/volmit/react/service/PlayerSVC.java
@@ -0,0 +1,29 @@
+package com.volmit.react.service;
+
+import com.volmit.react.api.RSVC;
+
+public class PlayerSVC extends RSVC
+{
+	public PlayerSVC()
+	{
+		super(true, 20, 50);
+	}
+
+	@Override
+	public void onStart()
+	{
+
+	}
+
+	@Override
+	public void onStop()
+	{
+
+	}
+
+	@Override
+	public void onTick()
+	{
+
+	}
+}
